e01c8ba51868f86e34530a548dc6b1ecf6abd22f
Damiprince <adamilare183@gmail.com>
2024-10-22 02:41:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// Initialize Particles.js for the animated background
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 100,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "shape": {
+            "type": "circle"
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 1,
+                "opacity_min": 0
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 40,
+                "size_min": 0.3
+            }
+        },
+        "links": {
+            "enable": false
+        },
+        "move": {
+            "enable": true,
+            "speed": 2
+        }
+    },
+    "interactivity": {
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            }
+        }
+    }
+});
